266426631219160c0438a93763f5202a470af92d
aarthi-mahathi <aarthi.m@mahathiinfotech.com>
2025-04-18 17:34:01 +0530

    create account form

--- src/app/create-account-form/create-account-form.component.ts
+++ src/app/create-account-form/create-account-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-account-form',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      // companyName: ['', Validators.required],
+      // firstName: ['', Validators.required],
+      // lastName: ['', Validators.required],
+      // phone: ['+1', [Validators.required]],
+      // email: ['', [Validators.required, Validators.email]],
+      // password: ['', Validators.required],
+      // confirmPassword: ['', Validators.required],
+      // agreeTerms: [false, Validators.requiredTrue],
+      // receiveEmails: [false],
+    });
+  }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Data:', this.form.value);
+    }
   }
 
 }
